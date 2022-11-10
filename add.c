a579c59351f15279ba527441601021d6e614d919
Group021 <Group021@infoway.com>
2022-11-10 18:23:05 +0530

    add to local regectory

---
+++ add.c
@@ -0,0 +1,20 @@
+/*#include<stdio.h> 
+main(){ 
+	int num1,num2,sum;
+	num1=10;
+	num2=20;
+	sum=num1+num2;
+	printf("Addition of %d and %d is %d",num1,num2,sum);
+				
+}*/
+
+#include<stdio.h>
+ main(){
+int num1,num2,num3 ,sum;
+    num1=10;
+    num2=36;
+    num3=45;
+    sum = num1+num2+num3;
+    printf("addition of %d and %d and %d is %d", num1,num2,num3,sum);
+    return 0;
+}
